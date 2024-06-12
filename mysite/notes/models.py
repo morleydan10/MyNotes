e4e8344c692437003ba6d2f30527067bc6780db8
Daniel Morley <morleydan10@gmail.com>
2024-06-12 15:40:52 -0400

    edited models

--- mysite/notes/models.py
+++ mysite/notes/models.py
@@ -4,9 +4,6 @@ from django.contrib.auth.models import User
 class Tag(models.Model):
     description = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.description
-
 class Note(models.Model):
     title = models.CharField(max_length=15)
     body = models.TextField()
@@ -15,6 +12,4 @@ class Note(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
+    
